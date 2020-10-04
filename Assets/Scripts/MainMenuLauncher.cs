99fa2e2ed87dbaff3c4f815f31fc8372dddc4bf4
Nirfiolo <bodyaufml@gmail.com>
2020-10-04 13:38:40 +0300

    Add TextMesh Pro, add base main menu

---
+++ Assets/Scripts/MainMenuLauncher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuLauncher : MonoBehaviour
+{
+    public MenuManager menuManager;
+
+
+    public Button temp;
+
+    private void Start()
+    {
+        menuManager.OpenMenu(Menu.MenuName.MainMenu);
+
+        temp.onClick.AddListener(() => Debug.Log("Click"));
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Menu[] menus;
+
+
+    public void OpenMenu(Menu menu)
+    {
+        OpenMenu(menu.menuName);
+    }
+
+    public void OpenMenu(Menu.MenuName menuName)
+    {
+        foreach (Menu menu in menus)
+        {
+            if (menu.menuName == menuName && !menu.isOpen)
+            {
+                menu.Open();
+            }
+            else if (menu.menuName != menuName &&  menu.isOpen)
+            {
+                menu.Close();
+            }
+        }
+    }
+}
