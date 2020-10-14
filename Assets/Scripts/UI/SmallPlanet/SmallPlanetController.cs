cd4381ffde115c7235a0015c3201df5adf5c595b
Nirfiolo <bodyaufml@gmail.com>
2020-10-14 20:28:58 +0300

    Add small planet prefab

---
+++ Assets/Scripts/UI/SmallPlanet/SmallPlanetController.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SmallPlanetController : MonoBehaviour
+{
+    public GameObject planet;
+    public Canvas canvas;
+    public TMP_Text planetName;
+    public TMP_Text select;
+
+    [HideInInspector]
+    public bool isSelected = false;
+    
+
+    public void SetMaterial(Material material)
+    {
+        planet.GetComponent<Renderer>().material = material;
+    }
+
+    public void SetCamera(Camera camera)
+    {
+        canvas.worldCamera = camera;
+    }
+
+    public void SetName(string name)
+    {
+        planetName.text = name;
+    }
+
+    public void Select()
+    {
+        isSelected = true;
+        select.text = "Selected";
+    }
+
+    public void Unselect()
+    {
+        isSelected = false;
+        select.text = "";
+    }
+}
---
+++ Assets/Scripts/UI/SmallPlanet/SmallPlanetRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SmallPlanetRotator : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
