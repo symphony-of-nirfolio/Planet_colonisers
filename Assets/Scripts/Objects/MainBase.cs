2a61b9dce1eb538d82e3f8dfbd04f2eb291c6244
Afohtim <afohmouth@gmail.com>
2020-11-07 00:44:08 +0200

    First base and resources, packs

---
+++ Assets/Scripts/Objects/MainBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBase : MonoBehaviour
+{
+    [SerializeField]
+    private GameResouces resouces;
+    private People people;
+    [SerializeField]
+    private bool isFirstInColony;
+    private List<InBaseObject> objects = new List<InBaseObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isFirstInColony)
+        {
+            people.AddPeople(50);
+            objects.Add(new InBaseMoxie());
+            objects.Add(new InBaseVaporator());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Objects/People.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class People
+{
+    public int PeopleCount { get; private set; } = 0;
+
+    public void AddPeople(int people)
+    {
+        Debug.Assert(people >= 0);
+        PeopleCount += people;
+    }
+
+    public void RemovePeople(int people)
+    {
+        Debug.Assert(people >= 0);
+        Debug.Assert(PeopleCount >= people);
+        PeopleCount -= people;
+    }
+}
