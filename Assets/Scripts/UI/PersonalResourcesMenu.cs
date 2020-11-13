13869db12fa313998242338f95181f887ee8eea0
Nirfiolo <bodyaufml@gmail.com>
2020-11-13 20:12:02 +0200

    Replace singles game resources to array

--- Assets/Scripts/UI/PersonalResourcesMenu.cs
+++ Assets/Scripts/UI/PersonalResourcesMenu.cs
@@ -7,59 +7,31 @@ public class PersonalResourcesMenu : MonoBehaviour
     // Temporary class to functional check
     public class GameResources
     {
-        public int resource1 = 0;
-        public int resource2 = 0;
-        public int resource3 = 0;
-        public int resource4 = 0;
+        public const int resourcesCount = 12;
 
-        public int resource5 = 0;
-        public int resource6 = 0;
-        public int resource7 = 0;
-        public int resource8 = 0;
-        
-        public int resource9 = 0;
-        public int resource10 = 0;
-        public int resource11 = 0;
-        public int resource12 = 0;
+        public int[] resources = new int[resourcesCount];
     }
 
-    public TMP_Text resource1;
-    public TMP_Text resource2;
-    public TMP_Text resource3;
-    public TMP_Text resource4;
-
-    public TMP_Text resource5;
-    public TMP_Text resource6;
-    public TMP_Text resource7;
-    public TMP_Text resource8;
-    
-    public TMP_Text resource9;
-    public TMP_Text resource10;
-    public TMP_Text resource11;
-    public TMP_Text resource12;
+    public TMP_Text[] resourceCountTexts;
 
 
     public void UpdateResources(GameResources gameResources)
     {
-        resource1.text = gameResources.resource1.ToString();
-        resource2.text = gameResources.resource2.ToString();
-        resource3.text = gameResources.resource3.ToString();
-        resource4.text = gameResources.resource4.ToString();
-
-        resource5.text = gameResources.resource5.ToString();
-        resource6.text = gameResources.resource6.ToString();
-        resource7.text = gameResources.resource7.ToString();
-        resource8.text = gameResources.resource8.ToString();
-        
-        resource9.text = gameResources.resource9.ToString();
-        resource10.text = gameResources.resource10.ToString();
-        resource11.text = gameResources.resource11.ToString();
-        resource12.text = gameResources.resource12.ToString();
+        for (int i = 0; i < GameResources.resourcesCount; ++i)
+        {
+            resourceCountTexts[i].text = gameResources.resources[i].ToString();
+        }
     }
 
     // TODO: remove it
     // Temporary variable to functional check
     private GameResources gameResources = new GameResources();
+
+    private void Start()
+    {
+        Debug.Assert(resourceCountTexts.Length == GameResources.resourcesCount,
+            "Resource Count Text array have to be same size as Game Resources array");
+    }
 
     // TODO: remove it
     // Temporary function to functional check
@@ -67,20 +39,10 @@ public class PersonalResourcesMenu : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            int index = Random.Range(0, 3);
+            int index = Random.Range(0, 12);
+            int resourceValue = Random.Range(1, 100);
 
-            if (index == 0)
-            {
-                ++gameResources.resource3;
-            }
-            else if (index == 1)
-            {
-                ++gameResources.resource6;
-            }
-            else if (index == 2)
-            {
-                ++gameResources.resource10;
-            }
+            gameResources.resources[index] += resourceValue;
 
             UpdateResources(gameResources);
         }
