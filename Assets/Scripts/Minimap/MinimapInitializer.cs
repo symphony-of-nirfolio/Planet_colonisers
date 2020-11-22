ad0694c920b365209d03eb2faa90e46cc61eedea
Nirfiolo <bodyaufml@gmail.com>
2020-11-22 14:31:07 +0200

    Add base minimap

---
+++ Assets/Scripts/Minimap/MinimapInitializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MinimapInitializer : MonoBehaviour
+{
+    public GameParameters gameParameters;
+    public Camera minimapCamera;
+
+
+    private void Start()
+    {
+        Debug.Assert(gameParameters, "Game Parameters doesn't set");
+        Debug.Assert(minimapCamera, "Minimap Camera doesn't set");
+
+        minimapCamera.orthographicSize = gameParameters.mapSize.height / 2f;
+    }
+}
