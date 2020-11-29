cc7b1e60b91ab706b8ed7db8beca7bdb25556e9f
Nirfiolo <bodyaufml@gmail.com>
2020-11-29 21:56:18 +0200

    Hotfix add missing files

---
+++ Assets/Scripts/ResourceDepositData/ResourceSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ResourceSprite : MonoBehaviour
+{
+    public LimitedMinedResourceInfoList limitedMinedResourceInfoList;
+    public SpriteRenderer spriteRenderer;
+
+
+    public void InitWithGameResourceType(GameResourceType gameResourceType)
+    {
+        foreach (LimitedMinedResourceInfo resourceInfo in
+            limitedMinedResourceInfoList.resourceInfos)
+        {
+            if (resourceInfo.gameResourceType == gameResourceType)
+            {
+                spriteRenderer.sprite = resourceInfo.sprite;
+                return;
+            }
+        }
+        Debug.LogError("Game Resource Type doesn't found in Limited Mined Resource Info List");
+    }
+
+
+    private void Start()
+    {
+        Debug.Assert(limitedMinedResourceInfoList, "Limited Mined Resource Info List doesn't set");
+        Debug.Assert(spriteRenderer, "Sprite Renderer doesn't set");
+    }
+}
