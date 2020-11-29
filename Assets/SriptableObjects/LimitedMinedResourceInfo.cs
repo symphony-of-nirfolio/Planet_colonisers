0aa8200724a044c5381ff4bcbf4b25fc4f00172e
Nirfiolo <bodyaufml@gmail.com>
2020-11-29 19:31:44 +0200

    Add limited resource info list

---
+++ Assets/SriptableObjects/LimitedMinedResourceInfo.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "LimitedMinedResourceInfo/LimitedMinedResourceInfo")]
+public class LimitedMinedResourceInfo : ScriptableObject
+{
+    public GameResourceType gameResourceType;
+    public Sprite sprite;
+}
