a6cbdcd573d7fcbdc39f70046e9b0935f37c85ab
Nirfiolo <bodyaufml@gmail.com>
2020-12-02 00:41:40 +0200

    Hotfix typo

--- Assets/Scripts/EnvironmentsParameters/EnvironmentHeightParameter.cs
+++ Assets/Scripts/EnvironmentsParameters/EnvironmentHeightParameter.cs
@@ -2,22 +2,22 @@
 
 public class EnvironmentHeightParameter : MonoBehaviour
 {
-    public Transform mountainTransform;
+    public Transform evironmentTransform;
 
     public float minHeight = 150f;
     public float maxHeight = 400f;
 
-
-    public void InitMountain(float heightCoefficient)
+    /// <param name="heightCoefficient">Have to be in range [0f : 1f]</param>
+    public void InitHeight(float heightCoefficient)
     {
-        Vector3 localScale = mountainTransform.localScale;
+        Vector3 localScale = evironmentTransform.localScale;
         localScale.z = minHeight + heightCoefficient * (maxHeight - minHeight);
-        mountainTransform.localScale = localScale;
+        evironmentTransform.localScale = localScale;
     }
 
 
     private void Start()
     {
-        Debug.Assert(mountainTransform, "Mountain Transform doesn't set");
+        Debug.Assert(evironmentTransform, "Mountain Transform doesn't set");
     }
 }
