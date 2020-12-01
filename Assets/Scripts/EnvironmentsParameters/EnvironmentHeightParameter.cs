6d7ecc9790ee5c047f89622f3bc6d1ce4cc10844
Nirfiolo <bodyaufml@gmail.com>
2020-12-01 23:46:58 +0200

    Add crater and mountain prefabs, add environment height parameter script

---
+++ Assets/Scripts/EnvironmentsParameters/EnvironmentHeightParameter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnvironmentHeightParameter : MonoBehaviour
+{
+    public Transform mountainTransform;
+
+    public float minHeight = 150f;
+    public float maxHeight = 400f;
+
+
+    public void InitMountain(float heightCoefficient)
+    {
+        Vector3 localScale = mountainTransform.localScale;
+        localScale.z = minHeight + heightCoefficient * (maxHeight - minHeight);
+        mountainTransform.localScale = localScale;
+    }
+
+
+    private void Start()
+    {
+        Debug.Assert(mountainTransform, "Mountain Transform doesn't set");
+    }
+}
